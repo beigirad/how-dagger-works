7f61101927eaa1d862b6794ae8ad34afa7770119
Farhad Beigirad <f.beigirad@gmail.com>
2022-01-12 03:12:32 +0330

    pass context to logger component

--- src/main/kotlin/ir/beigirad/logger/Logger.kt
+++ src/main/kotlin/ir/beigirad/logger/Logger.kt
@@ -1,6 +1,9 @@
 package ir.beigirad.logger
 
-class Logger {
+import ir.beigirad.dagger.util.Context
+
+class Logger(context: Context) {
+
     fun log(message: String) {
         println("[Logger]\t$message")
     }
