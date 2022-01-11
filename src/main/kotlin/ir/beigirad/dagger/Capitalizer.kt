38ec1999dc3c4178d3035988e430c751b9dcadca
Farhad Beigirad <f.beigirad@gmail.com>
2022-01-11 23:32:23 +0330

    @provide capitalizer from module

--- src/main/kotlin/ir/beigirad/dagger/Capitalizer.kt
+++ src/main/kotlin/ir/beigirad/dagger/Capitalizer.kt
@@ -1,9 +1,8 @@
 package ir.beigirad.dagger
 
 import java.util.Locale
-import javax.inject.Inject
 
-class Capitalizer @Inject constructor() {
+class Capitalizer {
     fun capitalize(name: String): String {
         return name.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
     }
