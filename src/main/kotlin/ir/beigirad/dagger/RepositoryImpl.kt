9632798ebff4e0a4b18d29b95cfddf148ec5d9ea
Farhad Beigirad <f.beigirad@gmail.com>
2022-01-12 00:14:54 +0330

    add a log for repository creation

--- src/main/kotlin/ir/beigirad/dagger/RepositoryImpl.kt
+++ src/main/kotlin/ir/beigirad/dagger/RepositoryImpl.kt
@@ -2,12 +2,17 @@ package ir.beigirad.dagger
 
 import ir.beigirad.dagger.qualifier.TypeA
 import ir.beigirad.dagger.util.Context
+import ir.beigirad.dagger.util.hashString
 import javax.inject.Inject
 
 class RepositoryImpl @Inject constructor(
     private val context: Context,
     @TypeA private val capitalizer: Capitalizer
 ) : Repository {
+    init {
+        println("Repository has created. ${this.hashString()}")
+    }
+
     override fun getUsersName(): List<String> {
         return listOf("mamad", "gholi").map { capitalizer.capitalize(it) }
     }
